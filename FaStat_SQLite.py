1b61a972300572a30acd6429c3bf705998cdf4c3
fr4ed <fr4ed@rustadholmboe.com>
2015-01-03 23:30:03 +0100

    Added ad column if not in table

--- FaStat_SQLite.py
+++ FaStat_SQLite.py
@@ -7,13 +7,25 @@ import sqlite3
 conn = sqlite3.connect('FaStat.Value.sqlite3')
 cur = conn.cursor()
 
-cur.execute('DROP TABLE IF EXISTS Value') # obs. SLETTER databasen!!!
+cur.execute('DROP TABLE IF EXISTS Value') # obs. SLETTER databasen!!! Må bort
 
-cur.execute('CREATE TABLE Value (ID INT PRIMARY KEY NOT NULL,ATC TEXT NOT NULL,ATC_NAME TEXT NOT NULL, Jan_2015 DATE, Des_2014 DATE)') # Lager databasen med disse kolonnene
+cur.execute('CREATE TABLE Value (ID INT PRIMARY KEY NOT NULL,ATC TEXT NOT NULL,ATC_NAME TEXT NOT NULL, Jan_2015 DATE, Des_2014 DATE)') # Lager databasen med disse kolonnene, burde holde med første gang!? Burde ATC være key, eller ikke så farlig?
+
+# Sjekk om kolonnen finnes allerede, hvis ikke legg til!
+try:
+    cur.execute('ALTER TABLE Value ADD COLUMN test_2014')
+except:
+    pass
+
+
 
 
 conn.commit() # Lagre endringer
 conn.close() # Lukk database-koblingen
+
+# En database for siste kjøring (36 mnd)
+# En database løpende per år??
+# En database per komplette år??
 
 # Populate one table using another table:
 
@@ -25,3 +37,9 @@ conn.close() # Lukk database-koblingen
 #   SELECT column1, column2, ...columnN 
 #   FROM second_table_name
 #   [WHERE condition];
+#
+# Dvs. som dette ish:
+#INSERT INTO Value [(Des_2014)] # Ny måned
+#   SELECT Des_2014 # Ny måned
+#   FROM Oppdatering_FaStat # Velge rett kolonne her og?
+#   [WHERE condition]; # Value.ATC = Oppdatering_FaStat.ATC
